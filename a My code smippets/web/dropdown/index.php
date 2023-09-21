273597d60ba5b002849e60136837bd1f8082f1f5
aldemas-py <Njengasam2406@gmail.com>
2023-09-21 16:03:18 +0300

    css updated in dropdown menu

--- a My code smippets/web/dropdown/index.php
+++ a My code smippets/web/dropdown/index.php
@@ -2,33 +2,65 @@
 <html>
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
-  <div class="container">
-    <div class="dropdown">
-      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-        <span class="caret"></span></button>
-      <ul class="dropdown-menu">
-        <li><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-          <label?<a href="#">HTML</a></label>
-        </li>
-        <li>
-          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-          <label?<a href="#">CSS</a></label>
-        </li>
-        <li>
-          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-          <label?<a href="#">JavaScript</a></label>
-        </li>
-      </ul>
+    <!-- this is the first dropdown -->
+    <div class="container">
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+                <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                    <label?<a href="#">HTML</a></label>
+                </li>
+                <li>
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                    <label?<a href="#">CSS</a></label>
+                </li>
+                <li>
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                    <label?<a href="#">JavaScript</a></label>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+
+    </head>
+
+    <body>
+
+        <div class="container">
+            <h2>Dropdowns</h2>
+            <p>The .dropdown class is used to indicate a dropdown menu.</p>
+            <p>Use the .dropdown-menu class to actually build the dropdown menu.</p>
+            <p>To open the dropdown menu, use a button or a link with a class of .dropdown-toggle and
+                data-toggle="dropdown".</p>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">CSS</a></li>
+                    <li><a href="#">JavaScript</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </body>
+
+    </html>
+
 
 </body>
 
